c73b6d67e1b7387c21d43ce31537f7b8163afec4
Ayush Shrirao <ayushshrirao16@gmail.com>
2025-01-05 20:59:38 +0530

    Update book.controller.js

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -1,11 +1,12 @@
 import Book from "../model/book.model.js";
 
-export const getBook = async(req, res) => {
+// Controller to get all books
+export const getBook = async (req, res) => {
     try {
-        const book = await Book.find();
-        res.status(200).json(book);
+        const books = await Book.find();
+        res.status(200).json(books);
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json(error);
+        console.error("Error fetching books:", error);
+        res.status(500).json({ message: "An error occurred while fetching books.", error });
     }
 };
